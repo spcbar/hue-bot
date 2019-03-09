b02d0c663e0303c67fdb5f0d08f627d4994197b3
Yuri Golikov <meetmatt@ya.ru>
2019-03-09 07:32:20 +0100

    Removed frontend

--- src/time.php
+++ src/time.php
@@ -49,23 +49,3 @@ function secondsToTime($seconds)
 
     return $formatted;
 }
-
-function secondsToArray($seconds)
-{
-    $now  = new \DateTime('@0');
-    $then = new \DateTime("@$seconds");
-
-    $interval = $now->diff($then);
-
-    $days    = $interval->format('%a');
-    $hours   = $interval->format('%h');
-    $minutes = $interval->format('%i');
-    $seconds = $interval->format('%s');
-
-    return [
-        $days < 10 ? '0' . $days : $days,
-        $hours < 10 ? '0' . $hours : $hours,
-        $minutes < 10 ? '0' . $minutes : $minutes,
-        $seconds < 10 ? '0' . $seconds : $seconds,
-    ];
-}
